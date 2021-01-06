3b7031b9f4dc75dafb04233270a10054bbd91d3e
Parth Ray <parthray16@gmail.com>
2021-01-06 11:05:03 -0800

    my name and bug fixes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,8 @@
-public class Hello {
+public class HelloWorld {
    public static void main(String[] args){
-      System.out.println("Hello World\n");
+      System.out.println("Hello World");
       System.out.println("I have been at Cal Poly for 3 years");
-      System.out.println("My name is Kaanan Kharwa\n");
+      System.out.println("My name is Kaanan Kharwa");
+      System.out.println("My name is Parth Ray");
    }
 }
